5b6feb2d23f884d9a42cca6a885d6e65e641b61c
Steve Konrad <skonrad@wingmail.net>
2019-09-26 20:48:24 +0200

    added composer.json and stub bundle

---
+++ bundle/IntProgFeatureFlagBundle.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @category   PHP
+ * @package    intprog
+ * @version    1
+ * @date       2019-09-23 07:23 pm
+ * @author     Konrad, Steve <skonrad@wingmail.net>
+ * @copyright  Copyright © 2019, Intense Programming
+ */
+
+declare(strict_types=1);
+
+namespace IntProg\FeatureFlagBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class IntProgFeatureFlagBundle.
+ *
+ * @package   IntProg\FeatureFlagBundle
+ * @author    Konrad, Steve <skonrad@wingmail.net>
+ * @copyright 2019 Intense Programming
+ */
+class IntProgFeatureFlagBundle extends Bundle
+{
+}
